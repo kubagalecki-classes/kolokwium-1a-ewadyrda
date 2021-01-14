8065b7fa55103bb5d91269197c82b6e91c1d1006
ewadyrda <73639161+ewadyrda@users.noreply.github.com>
2021-01-14 13:00:40 +0100

    Update zad1.hpp

--- include/zad1.hpp
+++ include/zad1.hpp
@@ -6,3 +6,18 @@
 
 // Zad1
 // tutaj definicja klasy Tagliatelle
+
+class Tagliatelle:public Makaron{
+private:
+    double L;
+    double W;
+    double R;
+    static const double C;
+public:
+    Tagliatelle(double l, double w, double r):L(l),W(w),R(r){}
+    Tagliatelle():L(0.5),W(0.5),R(0.5){}
+    const double ileMaki(unsigned P){
+       return(double) P*L*W*(1.-R)*C;
+    }
+
+};
