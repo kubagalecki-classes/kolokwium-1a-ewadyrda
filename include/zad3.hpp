a0cae017407bea0d5556f63d021c1bffcf9d9685
ewadyrda <73639161+ewadyrda@users.noreply.github.com>
2021-01-14 13:14:35 +0100

    Update zad3.hpp

--- include/zad3.hpp
+++ include/zad3.hpp
@@ -2,4 +2,7 @@
 #include <cstdint>
 #include <functional>
 
-// tutaj funkcja polejSosem
+template<typename E>
+std::size_t polejSosem (const Tagliatelle &e, const E &e2){
+    return e2.pole(2);
+}
